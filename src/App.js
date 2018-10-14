f93d849de42cbe2969afd46ecbebac5eb7993ad2
Taiming Chen <eefozeo@gmail.com>
2018-10-14 22:21:57 +0800

    add github corner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import TicTacToe from 'containers/TicTacToe';
+import GithubCorner from 'react-github-corner';
+import {
+  THEME_DARKEN,
+} from 'containers/StyleConstants';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <TicTacToe />
+        <GithubCorner bannerColor={THEME_DARKEN} href="https://github.com/TimingJL/tic-tac-toe" />
       </div>
     );
   }
